d967a3ad8be4b4be7bd5ada4ebf7955c78c8b2a3
Alex <alexjoy2009@gmail.com>
2022-07-16 08:13:40 +0530

    Added end points

--- app/src/main/java/com/example/guidemodernapparchitecture/network/EndPoints.kt
+++ app/src/main/java/com/example/guidemodernapparchitecture/network/EndPoints.kt
@@ -1,5 +1,6 @@
 package com.example.guidemodernapparchitecture.network
 
 object EndPoints {
-
+    const val API_KEY = "c7397337dfb7474caa976615ac923622"
+    const val TOP_HEADLINES = "/v2/top-headlines"
 }
